a012cd1750f152cb67fa41919b75d8868755dfde
timfallmk <tim@midokura.com>
2014-08-05 23:28:48 +0000

    fill in the ids

--- zerg-it.sh
+++ zerg-it.sh
@@ -2,8 +2,8 @@
 set -x
 
 TENANT_ID=c70abe88ef094f999adb4d3eb4d01725
-ROUTER_ID=
-IMAGE_ID=
+ROUTER_ID=8f67ee65-32c2-4191-9d43-5db7abcef9f1
+IMAGE_ID=125042ad-fe8f-48f1-9fb6-8c6ba19c2427
 
 # number of relays and networks to create
 iterations=20
