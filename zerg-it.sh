f33aa8cf332ee24af5799ed1483d1359011a4f3b
timfallmk <tim@midokura.com>
2014-08-06 00:25:36 +0000

    added the loop and logging

--- zerg-it.sh
+++ zerg-it.sh
@@ -7,13 +7,21 @@ IMAGE_ID=125042ad-fe8f-48f1-9fb6-8c6ba19c2427
 
 # number of relays and networks to create
 iterations=20
+echo "$(tput setaf 1)We're creating ${iterations} of these things$(tput sgr0)"
 
 source ./environment.rc
 
 
 # create a new network and subnet, then connect them to the router
-neutron net-create --tenant-id $TENANT_ID "relay network ${n}"
-neutron subnet-create --tenant-id $TENANT_ID --name "relay network ${n} subnet" --dns-nameserver 8.8.8.8 --enable-dhcp "relay network ${n}" 10.0.0.0/24
-neutron router-interface-add ${ROUTER_ID} "relay network ${n} subnet"
+for n in ${iterations}
+do
+	neutron net-create --tenant-id $TENANT_ID "relay network ${n}"
+	echo "$(tput setaf 1)Creating network ${n}$(tput sgr0)"
+	neutron subnet-create --tenant-id $TENANT_ID --name "relay network ${n} subnet" --dns-nameserver 8.8.8.8 --enable-dhcp "relay network ${n}" 10.0.0.0/24
+	echo "$(tput setaf 2)Creating a subent for network ${n}"
+	neutron router-interface-add ${ROUTER_ID} "relay network ${n} subnet"
+	echo "$(tput setaf 3)Attaching network ${n} to the router$(tput sgr0)"
 
-nova boot --flavor 10 --image ${IMAGE_ID} --security-groups relay --key-name tim-chromebook --nic <net-id=${}> "tor relay ${n}"
+	nova boot --flavor 10 --image ${IMAGE_ID} --security-groups Relay --key-name tim-chromebook --nic net-id=${} "tor relay ${n}"
+	echo "$(tput setaf 1)Booting relay #${n}$(tput sgr0)"
+done
